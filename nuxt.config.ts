d5e8f2f202c52b6e1e016d5aa03846c5a83f5610
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-06-28 15:55:33 +0900

    ESLintの導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
-});
+
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
