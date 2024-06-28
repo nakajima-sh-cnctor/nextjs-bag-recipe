8002f010e26ce62582d9edd5b0b7292fe41d02dc
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-06-28 16:44:38 +0900

    vuetifyの導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,41 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
+  build: {
+    transpile: ['vuetify'],
+  },
+  plugins: [
+    '~/plugins/vuetify.ts',
+  ],
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
 
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
   eslint: {
     config: {
       stylistic: true,
     },
   },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.css',
+  ],
 })
