2f81ca539ca877ec85c1c4dcc209dd50177512a6
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-07-01 10:23:24 +0900

    Storybookの導入

---
+++ utils/vuetify.ts
@@ -0,0 +1,19 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify: ReturnType<typeof createVuetify> = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+export default vuetify
