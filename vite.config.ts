786a340680c5416521929dcf31878330d8e708b1
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-07-01 10:08:53 +0900

    設定ファイルの追加と変更

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @stylistic/indent */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+
+    plugins: [vue()],
+})
