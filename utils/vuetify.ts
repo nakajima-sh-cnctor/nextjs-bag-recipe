65f6d3a8d90259ddbdfd84846a3b3bd097c819fc
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2024-07-04 11:49:17 +0900

    Storybook プレビューで Vuetify テンプレートを更新  vuetifyのclassの適応忘れの修正

--- utils/vuetify.ts
+++ utils/vuetify.ts
@@ -1,4 +1,5 @@
 import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
